cd23df7e21f43666111710913b23e99194ef8770
pedroinfante92 <166009636+pedroinfante92@users.noreply.github.com>
2025-11-28 13:58:05 +0100

    feat:Implementation of the Search component and management of the search term.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import hero from "../public/hero.png"
+import Search from "./components/Search";
+import { useState } from "react";
 
 const App = () => {
+    
+    const [searchTerm, setSearchTerm] = useState('')
+
     return(
         <main>
             <div className="pattern" />
             <div className="wrapper">
                 <header>
-                    <img src={hero} alt="Hero Banner" />
+                    <img src="./hero.png" alt="Hero Banner" />
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
                 </header>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
             </div>
-            <p>Search</p>
         </main>
     )
 }
