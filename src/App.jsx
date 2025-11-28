15bdd2c7947b49d7b73ee8b64af42b6d9b0ecdab
pedroinfante92 <166009636+pedroinfante92@users.noreply.github.com>
2025-11-28 14:46:19 +0100

    feat: add fetchMovies, useEffect to load popular movies, state management for movies, loading, and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,94 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Search from "./components/Search";
 import { useState } from "react";
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+  },
+};
+
 const App = () => {
-    
-    const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [movieList, setMovieList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
-    return(
-        <main>
-            <div className="pattern" />
-            <div className="wrapper">
-                <header>
-                    <img src="./hero.png" alt="Hero Banner" />
-                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
-                </header>
-            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-            </div>
-        </main>
-    )
-}
+  const fetchMovies = async () => {
 
-export default App
+    setIsLoading(true)
+    setErrorMessage('')
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+
+      console.log(data);
+
+      if(data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch Movies')
+        setMovieList([])
+        return
+      }
+
+      setMovieList(data.results || [])
+
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+
+      setErrorMessage("Error fetching movies. Please try again later");
+    } finally {
+      setIsLoading(false)
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+<section className="all-movies">
+  <h2>All Movies</h2>
+
+  {isLoading ? (
+    <p className="text-white">Loading</p>
+  ) : errorMessage ? (
+    <p className="text-red-500">{errorMessage}</p>
+  ) : (
+    <ul>
+      {movieList.map((movie) => (
+        <p key={movie.id} className="text-white">{movie.title}</p>
+      ))}
+    </ul>
+  )}
+</section>
+      </div>
+    </main>
+  );
+};
+
+export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,18 @@
-const Search = ({searchTerm, setSearchTerm}) => {
-    return(
-        <div className="search">
-            <div>
-                <img src="search.svg" alt="search" />
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="search" />
 
-                <input 
-                type="text"
-                placeholder="Search through thousands of movies"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                />
-            </div>
-        </div>
-    )
-}
+        <input
+          type="text"
+          placeholder="Search through thousands of movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
